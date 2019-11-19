50a124ce4a848a6685f4e24da0bc2c9f9ffd7a7b
Aleksandar Olic <aleksandar.olic@clockify.me>
2019-11-19 14:46:11 +0100

    Clickup integration: recognize project

--- src/integrations/clickup.js
+++ src/integrations/clickup.js
@@ -1,16 +1,15 @@
-'use strict';
-
 clockifyButton.render('#timeTrackingItem:not(.clockify)', {observe: true}, (elem) => {
+  var link,
+  description = $('.task-name', elem).textContent,
+  clockifyDiv = createTag('div', 'clockify-container'),
+  appendTo = $('.cu-task-info_time-tracking');
 
-    var link,
-        description = $('.task-name', elem).textContent,
-        clockifyDiv = createTag('div', 'clockify-container'),
-        appendTo = $('.cu-task-info_time-tracking');
+  project = $("div.task-container__header.cu-hidden-print > cu-task-breadcrumbs > div > a:nth-child(4) > span").textContent;
+  link = clockifyButton.createSmallButton(description, project);
 
-    link = clockifyButton.createSmallButton(description);
-    link.style.position = "relative";
-    link.style.top = "0px";
-    link.style.left = "15px";
-    clockifyDiv.appendChild(link);
-    appendTo.parentNode.insertBefore(clockifyDiv, appendTo);
+  link.style.position = "relative";
+  link.style.top = "0px";
+  link.style.left = "15px";
+  clockifyDiv.appendChild(link);
+  appendTo.parentNode.insertBefore(clockifyDiv, appendTo);
 });
