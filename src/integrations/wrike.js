57d3882a376608a44c35ed2639295fcd29a1283d
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2019-12-06 09:11:22 +0100

    Wrike integration: Fix

--- src/integrations/wrike.js
+++ src/integrations/wrike.js
@@ -1,10 +1,37 @@
-clockifyButton.render('.wrike-panel-header:not(.clockify)', {observe: true}, function (elem) {
-    var link,
-        container = $('.wrike-panel-header-toolbar', elem),
-        titleElem = function () {
-            return $('.title-field-ghost').textContent;
-        };
-    link = clockifyButton.createButton(titleElem);
+clockifyButton.render(
+  '.wspace-task-view:not(.clockify)',
+  { observe: true },
+  function (elem) {
+    const container = $('.wrike-panel-header-toolbar', elem);
+
+    const getTitleElement = function () {
+      const wsTaskTitle = document.querySelectorAll('ws-task-title');
+      if (wsTaskTitle.length === 1 && wsTaskTitle[0].textContent !== '') {
+        return wsTaskTitle[0];
+      }
+      return $('title');
+    };
+
+    const descriptionText = function () {
+      const urlString = window.location.href;
+      const url = new URL(urlString);
+      const hash = url.hash.substr(1);
+      const hashParts = hash.split('&');
+      let taskId = null;
+      for (let i = 0; i < hashParts.length; i++) {
+        const partSplit = hashParts[i].split('=');
+        if (partSplit[0] === 'ot') {
+          taskId = partSplit[1];
+        }
+      }
+
+      const titleElem = getTitleElement();
+      const titleElemText = titleElem ? titleElem.textContent : 'not found';
+      return `${taskId ? '#' + taskId : ''} ${titleElemText.trim().replace(' - Wrike', '')}`.trim();
+    };
+
+    const link = clockifyButton.createButton(descriptionText);
 
     container.insertBefore(link, container.firstChild);
-});
+  }
+);
