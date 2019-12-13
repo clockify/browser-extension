589c30c6343a705776a5653b1faba3fc9fcfab9b
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2019-12-13 13:00:04 +0100

    Zendesk integration: Fixed duplicate timers

--- src/integrations/zendesk.js
+++ src/integrations/zendesk.js
@@ -1,32 +1,28 @@
-'use strict';
+clockifyButton.render('.pane_header:not(.clockify)', { observe: true }, function (
+  elem
+) {
+  let description;
+  const projectName = $('title').textContent;
 
-clockifyButton.render('.pane_header:not(.clockify)', {observe: true}, function (elem) {
-    if(elem.classList.contains('web')) {
-        var link, titleFunc, description,
-            projectName = $('title').textContent,
-            divTag = document.createElement("div");
+  const titleFunc = function () {
+    const titleElem = $('.selected .tab_text .title');
+    const ticketNum = location.href.match(/tickets\/(\d+)/);
 
-        titleFunc = function () {
-            var titleElem = $('.selected .tab_text .title'),
-                ticketNum = location.href.match(/tickets\/(\d+)/);
-
-            if (titleElem !== null) {
-                description = titleElem.textContent.trim();
-            }
-
-            if (ticketNum) {
-                description = '#' + ticketNum[1].trim() + " " + description;
-            }
-            return description;
-        };
-
-        link = clockifyButton.createButton(titleFunc);
-        link.style.float = 'right';
-        link.style.marginRight = '30px';
-        link.style.marginTop = '-36px';
-        link.style.marginBottom = '-20px';
-        divTag.appendChild(link);
-        elem.insertBefore(divTag, elem.querySelector('.btn-group'));
+    if (titleElem !== null) {
+      description = titleElem.textContent.trim();
     }
 
+    if (ticketNum) {
+      description = '#' + ticketNum[1].trim() + ' ' + description;
+    }
+    return description;
+  };
+
+  const link = clockifyButton.createButton(titleFunc);
+
+  if (elem.querySelector('#clockifyButton')) {
+    elem.removeChild(elem.querySelector('#clockifyButton'));
+  }
+
+  elem.insertBefore(link, elem.querySelector('.btn-group'));
 });
