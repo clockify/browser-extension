22e998c29091c1e84a0580409957db9b4e8a40cc
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2019-12-17 09:14:53 +0100

    Outlook integration fix

--- src/integrations/outlook.js
+++ src/integrations/outlook.js
@@ -1,7 +1,23 @@
-clockifyButton.render('._3VCj_0E9bAt70Mt6wZNhIi:not(.clockify)', {observe: true}, (elem) => {
-    var link,
-        description = $('._1scEBBvYHbzuHObOs4tkqn', elem);
+// Inbox emails
+clockifyButton.render('[aria-label="Content pane"] [role="heading"] span:not(.clockify)', { observe: true }, elem => {
+  const container = elem.parentElement;
 
-    link = clockifyButton.createButton(description.textContent);
+  const link = clockifyButton.createButton(elem.textContent);
+  link.style.marginLeft = "10px";
+
+  container.appendChild(link);
+});
+
+// Composing emails
+clockifyButton.render('[aria-label="Command toolbar"] .ms-CommandBar-primaryCommand:not(.clockify)', { observe: true }, elem => {
+  const isComposingEmail = elem.querySelector('button[name="Send"]');
+
+  if (isComposingEmail) {
+    const subject = () => document.querySelector('[aria-label="Add a subject"]').value;
+
+    const link = clockifyButton.createButton(subject);
+    link.style.marginLeft = "10px";
+
     elem.appendChild(link);
+  }
 });
