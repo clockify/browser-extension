353e7703080cd07b0e140ea20cc2a0be46eff220
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2019-12-19 14:31:46 +0100

    Hubspot integration: improvements

--- src/integrations/hubspot.js
+++ src/integrations/hubspot.js
@@ -1,24 +1,40 @@
 // Inbox
 clockifyButton.render('[data-test-id="sticky-subject-header"]:not(.clockify)', { observe: true }, elem => {
-  const container = elem.parentElement;
 
-  const link = clockifyButton.createButton(elem.textContent);
+setTimeout(function(){ 
+
+  const container = elem.parentElement;
+  const subject = document.querySelector('[data-test-id="sticky-subject-header"]').textContent;
+  const ticketId = window.location.href.split('/')[6].replace("#reply-editor", "");
+  const contact = document.querySelector(".customer-data-sidebar--highlight .private-truncated-string__inner").textContent;
+  const description = "[#" + ticketId + "] " + subject + " (" + contact  + ")";
+
+  const link = clockifyButton.createButton(description);
   link.style.position = 'absolute';
   link.style.right = '0px';
   link.style.top = '45px';
 
-
   container.appendChild(link);
+  }, 500);
 });
 
 // Tickets
 clockifyButton.render('[data-selenium-test="highlightTitle"]:not(.clockify)', { observe: true }, elem => {
-  const container = elem.parentElement;
-  const company = document.querySelector("[data-selenium-test='company-chicklet-title'] span").textContent;
 
-  const link = clockifyButton.createButton(elem.textContent, company);
+setTimeout(function(){ 
+
+  const container = elem.parentElement;
+  const ticketId = window.location.href.split('/')[6].replace("/", "");
+
+if (document.querySelector(".width-100 a.private-link.uiLinkWithoutUnderline.uiLinkDark")) {
+  contact = document.querySelector(".width-100 a.private-link.uiLinkWithoutUnderline.uiLinkDark").textContent;
+  link = clockifyButton.createButton("[#" + ticketId + "] " + elem.textContent + " (" + contact + ")");
+} else {
+  link = clockifyButton.createButton("[#" + ticketId + "] " + elem.textContent);
+}
   link.style.fontSize = '16px';
   link.style.display = 'block';
 
   container.appendChild(link);
+  }, 500);
 });
