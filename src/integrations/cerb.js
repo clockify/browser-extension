61d11e64ed3b676e9b487ac491f73df7f778c99f
SiteHost <support@sitehost.co.nz>
2020-04-24 01:52:52 +1200

    Add Cerb integration. (#79)
    Add Cerb integration

---
+++ src/integrations/cerb.js
@@ -0,0 +1,14 @@
+clockifyButton.render('#widget99:not(.clockify)', {observe: true}, function (elem) {
+    // Find a custom element storing the description or use the conversation title as the description
+    const descriptionElement = document.getElementById('clockify-task-name');
+    const description = descriptionElement !== null
+        ? descriptionElement.innerText
+        : document.getElementsByClassName('cerb-subpage')[0].getElementsByTagName('h1')[0].innerText;
+
+    // Find a custom element storing the project name or leave the project name blank
+    const projectElement = document.getElementById('clockify-project-name');
+    const projectName = projectElement !== null ? projectElement.innerText : '';
+
+    const link = clockifyButton.createButton(description, projectName);
+    elem.appendChild(link);
+});
