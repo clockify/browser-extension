f3a0999aabf906284d1c25c48370e93d94c4d8e1
Caio Portela <caio_fabiio@outlook.com>
2020-04-23 10:58:28 -0300

    Improved Nela integration
    Co-authored-by: Caio Portela <caio.fabio@viasoft.com.br>

--- src/integrations/nela.js
+++ src/integrations/nela.js
@@ -21,3 +21,31 @@ clockifyButton.render('.modal-content .card-body .card-title:not(.clockify)', { 
 
   container.appendChild(link);
 });
+
+clockifyButton.render('.modal-content .modal-body.rounded-lg:not(.clockify)', { observe: true }, (elem) => {
+  if(document.getElementById('clockifyButton')) {
+    document.getElementById('clockifyButton').remove();
+  }
+
+  const container = elem.querySelector('h6');
+
+  const [alias] = container.innerText.trim().split(' ');
+  let task = document.getElementById('task-title').value;
+
+  if(task) {
+    task = task.trim();
+  }
+
+  // Try to find the project
+  let project = document.querySelector('.header-filter .card-body h5');
+  if(project) {
+    project = project.innerText.trim();
+  } else {
+    project = '';
+  }
+
+  const link = clockifyButton.createButton(`${alias}: ${task}`, project);
+  link.classList.add('position-relative', 'pl-3', 'ml-3');
+
+  container.appendChild(link);
+});
