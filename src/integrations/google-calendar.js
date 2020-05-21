96ed284b1c02349f254218ab33229756467ddc64
Babak Khalkhali Shandiz <babak.k.shandiz@gmail.com>
2020-05-21 19:05:19 +0430

    🎁 Add support for Google Calendar Task cards
    Signed-off-by: Babak Khalkhali Shandiz <babak.k.shandiz@gmail.com>

--- src/integrations/google-calendar.js
+++ src/integrations/google-calendar.js
@@ -1,11 +1,16 @@
-clockifyButton.render('#xDetDlg > div:not(.clockify)', {observe: true}, function (elem) {
+let selector = [
+  "#xDetDlg > div:not(.clockify)",                      // Event card.
+  "div[jsname=ssXDle] div[data-taskid]:not(.clockify)", // Task card.
+].join(",");
+
+clockifyButton.render(selector, { observe: true }, function (elem) {
   var link, description;
   description = $('[role="heading"]', elem).textContent;
   link = clockifyButton.createButton(description);
   link.style.display = "block";
-  link.style.cursor = 'pointer';
-  link.style.position = 'absolute';
-  link.style.top = '0px';
-  link.style.left = '30px';
+  link.style.cursor = "pointer";
+  link.style.position = "absolute";
+  link.style.top = "0px";
+  link.style.left = "30px";
   elem.appendChild(link);
 });
