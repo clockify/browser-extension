ad0588bac679f0377de9f333ae916c8376614e84
David Poetzsch-Heffter <git@poehe.de>
2020-05-26 16:43:05 +0200

    fixed bug when createObjects is not present in local store

--- src/helpers/project-helper.js
+++ src/helpers/project-helper.js
@@ -154,7 +154,7 @@ export class ProjectHelper {
                     return project;
                 }
 
-                if (JSON.parse(localStorageService.get('createObjects'), false)) {
+                if (JSON.parse(localStorageService.get('createObjects', false))) {
                     return projectService.createProject({
                         name: projectName,
                         color: "#03a9f4"
