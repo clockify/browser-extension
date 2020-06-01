7b7371f52a1c94f027c18fd4b5702feef58497ad
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2020-06-01 15:58:15 +0200

    Update asana.js

--- src/integrations/asana.js
+++ src/integrations/asana.js
@@ -1,34 +1,12 @@
-    setTimeout(() => {
-    clockifyButton.render('.SingleTaskPane:not(.clockify)', {observe: true}, (elem) => {
-        var link,
-            container = $('.SingleTaskPaneToolbar-leftItems', elem),
-            description = $('.SingleTaskPane-titleRowInput > div > textarea', elem) ?
-                $('.SingleTaskPane-titleRowInput > div > textarea', elem).textContent : "",
-          tags = () => Array.from($$(".TaskTags-labeledRowStructure .PotTokenizerPillBase-name")).map(e => e.innerText),
-            projectElements = document.getElementsByClassName('PotTokenizerPillBase-name'),
-            project = projectElements && projectElements.length > 0 ?
-                projectElements[0].textContent : "";
-        link = clockifyButton.createButton({
-            description: description,
-            projectName: project,
-            taskName: description,
-            tagNames: tags
-        });
-        link.style.marginLeft = "10px";
-        container.appendChild(link);
-
-    });
-}, 1000);
-
 // New task pane list detail modal
 setTimeout(() => {
     clockifyButton.render('.SingleTaskPaneSpreadsheet:not(.clockify)', {observe: true}, (elem) => {
         var link,
-          tags = () => Array.from($$(".TaskTags-labeledRowStructure .PotTokenizerPillBase-name")).map(e => e.innerText),
+          tags = () => Array.from($$(".TokenizerPillBase-name")).map(e => e.innerText),
             container = $('.SingleTaskPaneToolbarAnimation-row', elem),
             description = $('.SingleTaskTitleInput-taskName textarea', elem) ?
                 $('.SingleTaskTitleInput-taskName textarea', elem).textContent : "",
-            projectElements = document.getElementsByClassName('PotTokenizerPillBase-name'),
+            projectElements = document.getElementsByClassName('TokenizerPillBase-name'),
             project = projectElements && projectElements.length > 0 ?
                 projectElements[0].textContent : "";
         link = clockifyButton.createButton({
@@ -41,13 +19,13 @@ setTimeout(() => {
         container.appendChild(link);
 
     });
-},100);
+},500);
 
 // subtasks
 setTimeout(() => {
   clockifyButton.render('.ItemRowTwoColumnStructure-left:not(.clockify)', {observe: true}, (elem) => {
-      projectElements = document.getElementsByClassName('PotTokenizerPillBase-name'),
-      tags = () => Array.from($$(".TaskTags-labeledRowStructure .PotTokenizerPillBase-name")).map(e => e.innerText),
+      projectElements = document.getElementsByClassName('TokenizerPillBase-name'),
+      tags = () => Array.from($$(".TokenizerPillBase-name")).map(e => e.innerText),
       maintask = $('.SingleTaskTitleInput-taskName textarea') ?  $('.SingleTaskTitleInput-taskName textarea').textContent : "", 
       project = projectElements && projectElements.length > 0 ?
                 projectElements[0].textContent : "";
