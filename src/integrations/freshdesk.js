f4177c5dca08175b70b8080e8c7b5d7a6b7e61ac
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2020-06-15 14:30:03 +0200

    Update freshdesk.js

--- src/integrations/freshdesk.js
+++ src/integrations/freshdesk.js
@@ -1,30 +1,15 @@
-clockifyButton.render('#Pagearea:not(.clockify)', { observe: true }, (elem) => {
-  var link,
-    container = createTag('li', 'ticket-btns'),
-    description,
-    titleElem = $('h2.subject', elem),
-    idElem = $('#ticket-display-id'),
-    projectElem = $('.logo_text'),
-    buttonsElem = $('.ticket-actions > ul');
-
-  description = idElem.textContent.trim() + ' ' + titleElem.textContent.trim();
-
+// New June 2020
+setTimeout(() => {
+  clockifyButton.render('.ticket-subject-heading:not(.clockify)', {observe: true}, function (elem) {
+    var link, description;
+    description = document.title.split(' : ')[0];
     link = clockifyButton.createButton(description);
-
-    container.appendChild(link);
-    buttonsElem.appendChild(container, buttonsElem);
-});
-
-// Freshdesk mint (late 2018)
-clockifyButton.render('.page-actions__left:not(.clockify)', { observe: true }, (elem) => {
-  var descriptionElem = $('.ticket-subject-heading');
-
-  // if there's no description element it's overview page, don't show
-  if (!descriptionElem) { return }
-
-  var ticketNumber = $('.breadcrumb__item.active').textContent.trim(),
-  subject = $('.ticket-subject-heading').textContent.trim(),
-
-  link = clockifyButton.createButton("[#" + ticketNumber + "] " + subject);
-  elem.appendChild(link);
-});
+    link.style.display = "block";
+    link.style.paddingTop = "0";
+    link.style.paddingBottom = "0";
+    link.style.marginBottom = "10px";
+    link.style.marginTop = "10px";
+    link.style.cursor = 'pointer';
+    elem.appendChild(link);
+  });
+},500);
