8505e3f5cc9b43596885ada63887fc808384b3f4
André M. Faria <andre-faria@users.noreply.github.com>
2020-06-18 12:34:20 -0300

    Update freshdesk.js (#99)
    Added "[" and "]" to ticket number.

--- src/integrations/freshdesk.js
+++ src/integrations/freshdesk.js
@@ -1,7 +1,7 @@
 clockifyButton.render('.page-actions__left:not(.clockify)', { observe: true }, function (elem) {
         const desc = $(".ticket-subject-heading").innerText;
         const ticket = $(".breadcrumb__item.active").innerText;
-        const link = clockifyButton.createButton("#" + ticket + " " + desc);
+        const link = clockifyButton.createButton("[#" + ticket + "] " + desc);
         link.style.marginLeft = "10px";
         elem.append(link);
 });
