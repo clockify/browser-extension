f6b3fa4b5f28510e4eda02374a1c33857d16c152
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2020-07-30 11:37:05 +0200

    Update freedcamp.js

--- src/integrations/freedcamp.js
+++ src/integrations/freedcamp.js
@@ -1,3 +1,25 @@
+// list new ui
+clockifyButton.render('.FastTaskItemComponent--fk-FastTaskItemComponent-MainContent:not(.clockify)', {observe: true}, (elem) => {
+    let project = $('.project_name').textContent.trim();
+    let description = $('.FastTaskItemComponent--fk-FastTaskItemComponent-ItemTitle', elem).textContent.trim();
+    let link = clockifyButton.createSmallButton(description, project);
+    link.style.paddingTop = "0px";
+    link.style.paddingRight = "10px";
+    elem.parentNode.prepend(link);
+});
+
+// task view new ui
+clockifyButton.render('#mainItemWrap:not(.clockify)', {observe: true}, (elem) => {
+    let project = $('#project_name .project_name').textContent.trim();
+    let description = $('.ItemView--fk-Item-Title').textContent.trim();
+    let link = clockifyButton.createButton(description, project);
+    link.style.paddingTop = "10px";
+    link.style.paddingLeft = "20px";
+    elem.parentNode.prepend(link);
+});
+
+// OLD UI
+
 // sidbar task
 clockifyButton.render('.al_container .item-title-group:not(.clockify)', {observe: true}, (elem) => {
     let description = $('h3').textContent.trim();
@@ -12,7 +34,8 @@ clockifyButton.render('.td_content:not(.clockify)', {observe: true}, (elem) => {
     let description = $('.td_description', elem).textContent.trim();
     let link = clockifyButton.createSmallButton(description, project);
     link.style.paddingTop = "0px";
-    elem.parentNode.appendChild(link);
+    link.style.paddingTop = "10px";
+    elem.parentNode.prepend(link);
 });
 
 // full width view
@@ -20,17 +43,6 @@ clockifyButton.render('.body_width .item-title-group:not(.clockify)', {observe: 
     let project = $('.project_name').textContent.trim();
     let description = $('h3').textContent.trim();
     let link = clockifyButton.createButton(description, project);
-    elem.parentNode.appendChild(link);
-});
-
-// kanban
-clockifyButton.render('.todo_wrapper a:not(.clockify)', {observe: true}, (elem) => {
-    let project = $('.project_name').textContent.trim();
-    let description = $('.todo_wrapper .text', elem).textContent.trim();
-    let link = clockifyButton.createSmallButton(description, project);
-    link.style.position = "relative";
-    link.style.top = "-42px";
-    link.style.right = "0px";
     elem.parentNode.appendChild(link);
 });
 
