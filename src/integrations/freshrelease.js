fac02c0fc3c4593222ceed04b47e5b1d16a5ddb7
Matthew Van der Bijl <matthewvdbijl@gmail.com>
2020-08-26 09:40:00 +0200

    Integrate with freshrelease tasks (#118)
    - Add timer button to agile board cards
    - Add timer button to card details side modal
    - Add timer button to card view page
    
    Branch: feat/freshrelease-integration

---
+++ src/integrations/freshrelease.js
@@ -0,0 +1,46 @@
+// Aglie board, largely a copy of the freshworks integration
+clockifyButton.render('.entity-wrap:not(.clockify)', { observe: true }, function (elem) {
+
+  const desc = $(".entity-title", elem).innerText;
+  const ticket = $(".entity-key", elem).innerText;
+
+  const link = clockifyButton.createButton("[" + ticket + "] " + desc);
+
+  link.style.display = "inline-flex";
+  link.style.verticalAlign = "middle";
+  link.style.float = "right";
+  link.style.paddingRight = "4px";
+
+  $(".entity--header", elem).append(link);
+});
+
+// Card view, eg *://*.freshrelease.com/ABC/tasks/ABC-123
+clockifyButton.render('.app-container:not(.clockify)', { observe: true }, function (elem) {
+
+  const desc = $(".title", elem).innerText;
+  const ticket = $(".page-action__left", elem).innerText.replace("Tasks ", "");
+
+  const link = clockifyButton.createButton("[" + ticket + "] " + desc);
+
+  link.style.display = "inline-flex";
+  link.style.verticalAlign = "middle";
+  link.style.paddingTop = "10px";
+  link.style.float = "right";
+
+  $('.title', elem).append(link);
+});
+
+// Agile board card side modal
+clockifyButton.render('.ember-modal-header:not(.clockify)', { observe: true }, function (elem) {
+
+  const desc = $(".form-inline", elem).innerText;
+  const ticket = $(".entity-key", elem).innerText;
+
+  const link = clockifyButton.createButton("[" + ticket + "] " + desc);
+
+  link.style.display = "inline-flex";
+  link.style.verticalAlign = "middle";
+  link.style.float = "right";
+
+  $(".summary-title--content", elem).append(link);
+});
