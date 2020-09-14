fe394e10e690050d65b965c69d798d20ce7361e8
Cedomir Mijajlovic <cedomir.mijajlovic@coing.rs>
2020-09-14 11:07:49 +0200

    Fix: Zendesk integration fix, button rendering fix

--- src/integrations/zendesk.js
+++ src/integrations/zendesk.js
@@ -26,3 +26,20 @@ setTimeout(() => {
     elem.insertBefore(link, elem.querySelector('.btn-group'));
   });
 }, 1000)
+
+setTimeout(() => {
+  clockifyButton.render(
+    'input[data-test-id="omni-header-subject"]:not(.clockify)',
+    { observe: true},
+    (elem) => {
+    const ticketNum = location.href.match(/tickets\/(\d+)/);
+    const description = 
+      ticketNum ? "#" + ticketNum[1].trim() + " " + elem.value.trim() : 
+                  elem.value.trim()
+  
+    const link = clockifyButton.createButton(description);
+    
+    elem.parentElement.prepend(link);
+    clockifyButton.disconnectObserver();
+  });
+}, 1000);
