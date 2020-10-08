187e0932990f98825dc5235c6f4c682f6f943fd9
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2020-10-08 12:15:48 +0200

    Update clickup.js

--- src/integrations/clickup.js
+++ src/integrations/clickup.js
@@ -1,18 +1,15 @@
-clockifyButton.render('.task-container .task-column__body-toolbar:not(.clockify)', {observe: true}, function (elem) {
-
-  var descriptionSelector = 'div.task-name-block > cu-slash-command > div',
-      projectSelector = 'cu-task-breadcrumbs > div > a.breadcrumbs__link.breadcrumbs__link_folder.ng-star-inserted > span',
-      tagSelector ='cu-tags-list > div > div > cu-tags-view > div div.cu-tags-select__name',
+clockifyButton.render('.task-container__header:not(.clockify)', {observe: true}, function (elem) {
+  var projectSelector = 'cu-task-breadcrumbs > div > a.breadcrumbs__link.breadcrumbs__link_folder.ng-star-inserted > span',
+      tagSelector ='.task-container.ng-trigger.ng-trigger-loading div.cu-tags-select__name',
       taskSelector = 'cu-task-breadcrumbs > div > a.breadcrumbs__link.breadcrumbs__link_last.breadcrumbs__link_list.ng-star-inserted > span';
     
   var link,
-    description = $(descriptionSelector) ? $(descriptionSelector).textContent : "",
-    task = $(taskSelector) ? $(taskSelector).textContent : "",
-    project = $(projectSelector).textContent,
-    tags = () => [...new Set(Array.from($$(tagSelector)).map(e => e.innerText))];
+    task = $(projectSelector) ? ($(taskSelector) ? $(taskSelector).textContent : "") : "",
+    project = $(projectSelector) ? $(projectSelector).textContent : ($(taskSelector) ? $(taskSelector).textContent : ""),
+    tags = $(tagSelector) ? () => [...new Set(Array.from($$(tagSelector)).map(e => e.innerText))] : "";
 
   link = clockifyButton.createButton({
-    description: description,
+    description: document.title,
     projectName: project,
     tagNames: tags,
     taskName: task
