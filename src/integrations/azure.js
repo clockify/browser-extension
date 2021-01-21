d99b60527cfcbb463689081d660324c14e8aff1b
BobBuildingCode <71820805+BobBuildingCode@users.noreply.github.com>
2021-01-21 15:13:04 +0100

    Clockify field project name & button element fix

--- src/integrations/azure.js
+++ src/integrations/azure.js
@@ -1,8 +1,8 @@
-clockifyButton.render('.work-item-form-headerContent:not(.clockify)', {observe: true}, function (elem) {
+clockifyButton.render('.work-item-form-headerContent:not(.clockify,.flex-row)', {observe: true}, function (elem) {
   var link, itemId, description, project;
   itemId =  $('.work-item-form-id > span', elem).textContent;
   description = $('.work-item-form-title input', elem).value;
-  project = $(".navigation-container .project-item .text-ellipsis").textContent;
+  project = $("input[aria-label='Clockify Project']") ? $("input[aria-label='Clockify Project']").value : $(".navigation-container .project-item .text-ellipsis").textContent;
   link = clockifyButton.createButton("#" + itemId + " " + description, project);
   link.style.display = "block";
   link.style.paddingTop = "0";
