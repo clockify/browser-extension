2210d077d27ac0450c262ace8ea46133d545a1c4
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2021-01-22 10:44:16 +0100

    Update azure.js

--- src/integrations/azure.js
+++ src/integrations/azure.js
@@ -3,7 +3,11 @@ clockifyButton.render('.work-item-form-headerContent:not(.clockify,.flex-row)', 
   itemId =  $('.work-item-form-id > span', elem).textContent;
   description = $('.work-item-form-title input', elem).value;
   project = $("input[aria-label='Clockify Project']") ? $("input[aria-label='Clockify Project']").value : $(".navigation-container .project-item .text-ellipsis").textContent;
-  link = clockifyButton.createButton("#" + itemId + " " + description, project);
+  link = clockifyButton.createButton({
+      description: "#" + itemId + " " + description,
+      projectName: project,
+      taskName: description,
+  });
   link.style.display = "block";
   link.style.paddingTop = "0";
   link.style.paddingBottom = "0";
