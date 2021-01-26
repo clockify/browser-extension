300c41b386a3dacf96b1f1caf563becb3e75e312
Rost <rosman12@gmail.com>
2021-01-26 10:30:04 +0200

    Group&work integration (#140)
    Co-authored-by: Andrii Boiko <shuganlive@gmail.com>

---
+++ src/integrations/groupnwork.js
@@ -0,0 +1,18 @@
+clockifyButton.render('#clockify-view:not(.clockify)', {observe: true}, (elem) => {
+    var container = $('#clokify-container', elem);
+    if (!container){
+       return; 
+    }
+
+    var description = $('#task-name');
+    description = description ? description.value : '';
+    var project = $('#board-title');
+    project = project ? project.textContent : '';
+    var link = clockifyButton.createButton({
+        description: description,
+        projectName: project,
+        taskName: description
+    });
+
+    container.appendChild(link);
+});
