fc7bb4959a8ef81ed7cdd4f7453c8bb95995ab6c
fabio de simone <prototypefab@gmail.com>
2021-02-20 15:48:54 +0100

    added input for details and sprint page

--- src/integrations/taiga.js
+++ src/integrations/taiga.js
@@ -1,18 +1,29 @@
+
 /* Epic/User story/Task/Issue details button */
 clockifyButton.render('.detail-title-wrapper:not(.clockify)', {observe: true}, (elem) => {
     var link,
+        input,
         projectElem = $('div.sticky-project-menu > tg-legacy-loader').shadowRoot.querySelector('span.project-name'),
         refElem = $('.detail-ref', elem),
         titleElem = $('.detail-subject', elem);
         taskName = refElem.textContent.trim() + ' ' + titleElem.textContent;
-        link = clockifyButton.createButton({
+    link = clockifyButton.createButton({
             description: taskName,
             projectName: projectElem.textContent.trim(),
             taskName: taskName,
             small: true
         });
-        link.style.marginRight = ".4em"
-    elem.insertBefore(link, $('.detail-title-text', elem));
+    input = clockifyButton.createInput({
+            description: taskName,
+            projectName: projectElem.textContent.trim(),
+            taskName: taskName,
+        });
+    link.style.marginRight = ".4em"
+    input.style.marginRight = ".4em"
+    input.style.fontSize = "small"
+    // elem.insertbefore(link, $('.detail-title-text', elem));
+    elem.append(link);
+    elem.insertBefore(input, link)
 });
 
 /* Epics Dashboard */
@@ -80,6 +91,7 @@ clockifyButton.render('.kanban .card-title:not(.clockify)', {observe: true}, (el
 clockifyButton.render('.taskboard .card-title:not(.clockify)', {observe: true}, (elem) => {
 
     var link,
+        input,
         refElem = $('.card-title > a > span:nth-child(1)', elem),
         titleElem = $('.card-title > a > span:nth-child(2)', elem),
         projectElem = $('div.sticky-project-menu > tg-legacy-loader').shadowRoot.querySelector('span.project-name'),
@@ -92,14 +104,21 @@ clockifyButton.render('.taskboard .card-title:not(.clockify)', {observe: true}, 
             taskName: taskName,
             small: true
         });
+    input = clockifyButton.createInput({
+            description: taskName,
+            projectName: projectElem.textContent.trim(),
+            taskName: taskName,
+        });
     link.style.flexGrow = 0;
 
-    /*change display from flex to inline-flex to put the button inline with the task link*/
-    link.style.display = "inline-flex"
-    $('.card-title > a', elem).style.display = "inline-flex"
+    link.style.display = "inline"
     link.style.marginRight = ".2em"
-
+    input.style.display = "inline"
+    input.style.marginRight = ".2em"
+    input.style.height = "25px"
+    input.style.fontSize = "small"
     elem.insertBefore(link, $('a', elem));
+    elem.insertBefore(input, link)
 });
 
 
@@ -110,16 +129,16 @@ clockifyButton.render('.row:not(.title) > div.subject:not(.clockify)', {observe:
         refElem = $('.issue-text > span:nth-child(1)', elem),
         titleElem = $('.issue-text > span:nth-child(2)', elem),
         taskName = refElem.textContent.trim() + ' ' + titleElem.textContent;
-        link = clockifyButton.createButton({
+    link = clockifyButton.createButton({
             description: taskName,
             projectName: projectElem.textContent.trim(),
             taskName: taskName,
             small: true
         });
-        /*change display from flex to inline-flex to put the button inline with the task link*/
-        link.style.display = "inline-flex"
-        $('a', elem).style.display = "inline-flex"
-        link.style.marginRight = ".2em"
+    /*change display from flex to inline-flex to put the button inline with the task link*/
+    link.style.display = "inline-flex"
+    $('a', elem).style.display = "inline-flex"
+    link.style.marginRight = ".2em"
     elem.prepend(link);
 });
 
