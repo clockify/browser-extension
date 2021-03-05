746deed2b85fc3a45b4b040370d19e7f18474ce2
twlcek <tim.wlcek@smarter-ecommerce.com>
2021-03-05 12:30:56 +0100

    Add id of salesforce object to description (#148)

--- src/integrations/salesforce.js
+++ src/integrations/salesforce.js
@@ -1,7 +1,15 @@
-if ((window.location.href.indexOf("/o/") == -1)) {
+if ((window.location.href.indexOf("/o/") == -1)) {  
   clockifyButton.render('.slds-page-header__title:not(.clockify)', {observe: true}, (elem) => {
-    setTimeout(() => {
-      link = clockifyButton.createButton(document.title.replace(" | Salesforce", ""));
+    setTimeout(() => {      
+      var description;
+      var objectIdRegex = /\b[a-z0-9]\w{4}0\w{12}|[a-z0-9]\w{4}0\w{9}\b/;
+      var matches = window.location.href.match(objectIdRegex);    
+      if (matches.length > 0) {
+        description = "#" + matches[0] + " ";
+      }
+      
+      description += document.title.replace(" | Salesforce", "")
+      link = clockifyButton.createButton(description);
       if (elem.querySelector('#clockifyButton')) {
         elem.removeChild(elem.querySelector('#clockifyButton'));
       }
