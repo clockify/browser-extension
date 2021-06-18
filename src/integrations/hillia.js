2d1ddb31a7a3e3c1fd30d79296388c1cde064430
Michał Matoga <michal.matoga@schibsted.pl>
2021-06-18 15:26:43 +0200

    Add hillia.app integration

---
+++ src/integrations/hillia.js
@@ -0,0 +1,19 @@
+clockifyButton.render(
+  ".task-list-item:not(.clockify):not(.flex-grow)",
+  { observe: true },
+  function (elem) {
+    const projectName = $("h2.montserrat.flex").innerText;
+    const taskName =
+      elem.parentNode.previousSibling.querySelector("input").value;
+    const description = elem.querySelector("input").value;
+    const link = clockifyButton.createButton({
+      projectName,
+      taskName,
+      description,
+    });
+    link.style.width = "120px";
+    link.style.margin = "0 10px";
+
+    elem.append(link);
+  }
+);
