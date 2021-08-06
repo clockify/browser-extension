d86079251081dd53b77f82d0511c52e49d47a6b9
Mistreanu Ionut <ionutmistreanu@gmail.com>
2021-08-06 10:27:23 +0300

    Add myfocusspace.com integration

---
+++ src/integrations/myfocusspace.js
@@ -0,0 +1,8 @@
+clockifyButton.render('#clockify_zone:not(.clockify)', {observe: true}, function (elem) {
+  var link, description;
+  description = elem.parentNode.innerText
+
+  link = clockifyButton.createSmallButton(description);
+  link.style.padding = "10px";
+  elem.appendChild(link);
+});
