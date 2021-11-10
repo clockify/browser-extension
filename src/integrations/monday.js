e9d85987d8614bb65a3121f776b10d97bf857fd9
maria-sisto <tori.akai@gmail.com>
2021-11-10 17:55:12 +0100

    Add task support for monday integration (#169)
    * fix monday button inclusion in pulse from standard board
    
    * fix monday button inclusion in pulse from my work board
    
    * fix monday button inclusion in pulse from kanban board
    
    * Display clickify popup on top of everything
    
    Co-authored-by: Maria Sisto <ms@digitalkingdom.ch>

--- src/integrations/monday.js
+++ src/integrations/monday.js
@@ -1,15 +1,30 @@
-clockifyButton.render('.flexible-header:not(.clockify)', {observe: true}, function (elem) {
+var style = document.createElement('style');
+style.innerHTML = `
+  #divClockifyPopupDlg.clockify-popup-dlg{
+    z-index: 100000000;
+  }
+#divClockifyTagDropDownPopup{
+    z-index: 100000000;
+  }
+#divClockifyProjectDropDownPopup{
+    z-index: 100000000;
+}
+  `;
+document.head.appendChild(style);
+
+// Pulse from standard board
+clockifyButton.render('.flexible-header:not(.clockify)', { observe: true }, function (elem) {
     let projectElem = $("#board-header > div.board-header-content-wrapper > div.board-header-main > div.board-header-top > div.board-header-left > div > div.ds-editable-component > div > span");
     if (!projectElem) {
         projectElem = $(".current-board-component div.board-name");       
     }
 
-    const project = projectElem ? projectElem.textContent : "";
+    var projectRefs = GetProjectAndTask(projectElem);
     const descriptionElem = $(".title-editable", elem);
     
     if (descriptionElem) {
         const description = () => descriptionElem ? descriptionElem.textContent : "";
-        link = clockifyButton.createButton(description, project);
+        link = clockifyButton.createButton(description, projectRefs.project, projectRefs.task);
         link.style.position = "absolute";
         link.style.top = "5px";
         link.style.right = "5px";
@@ -17,28 +32,40 @@ clockifyButton.render('.flexible-header:not(.clockify)', {observe: true}, functi
     }
 });
 
-clockifyButton.render('.pulse-page-header-component .pulse-page-name-wrapper:not(.clockify)', {observe: true}, function (elem) {
+// Pulse from "my word" board
+clockifyButton.render('.pulse-page-header-component .pulse-page-name-wrapper:not(.clockify)', { observe: true }, function (elem) {
     const descriptionElem = () => $(".ds-text-component > span", elem);
     const description = () => descriptionElem() ? descriptionElem().textContent : "";
-    const projectElem = $("#board-header > div.board-header-content-wrapper > div.board-header-main > div.board-header-top > div.board-header-left");
-    const project = projectElem ? projectElem.textContent : "";
-    link = clockifyButton.createButton(description, project);
+    const projectElem = $("div.pulse-page-header-component > div.link-to-pulse > .open-pulse-in-board-link");
+    var projectRefs = GetProjectAndTask(projectElem);
+    link = clockifyButton.createButton(description, projectRefs.project, projectRefs.task);
     link.style.position = "absolute";
     link.style.top = "5px";
     link.style.left = "60px";
     elem.appendChild(link);
 });
 
-// 
-clockifyButton.render('#pulse-card-dialog-component:not(.clockify)', {observe: true}, function (elem) {
+// Pulse from kanban board
+clockifyButton.render('#pulse-card-dialog-component:not(.clockify)', { observe: true }, function (elem) {
     const descriptionElem = () => $(".pulse-card-header .ds-text-component > span", elem);  //
     const description = () => descriptionElem() ? descriptionElem().textContent : "";
-    const projectElem = $("#board-header > div.board-header-content-wrapper > div.board-header-main > div.board-header-top > div.board-header-left");
-    const project = projectElem ? projectElem.textContent : "";
-    link = clockifyButton.createButton(description, project);
+    const projectElem = $("#pulse-card-dialog-scrollable-wrapper > div.pulse-card-header > div.pulse-data > div.link-to-pulse > .open-pulse-in-board-link");
+    var projectRefs = GetProjectAndTask(projectElem);
+    link = clockifyButton.createButton(description, projectRefs.project, projectRefs.task);
     link.style.position = "absolute";
     link.style.top = "5px";
     link.style.left = "60px";
     elem.appendChild(link);
 });
 
+function GetProjectAndTask(projectElem) {
+    var projName = projectElem ? projectElem.textContent : "";
+    var taskName = "";
+
+    if (typeof projName == "string" && projName.indexOf(":") > -1) {
+        var pNames = projName.split(":");
+        projName = pNames[0];
+        taskName = pNames[1];
+    }
+    return { project: projName, task: taskName };
+}
