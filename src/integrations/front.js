15f3e4760fc3487fd677bb32d8d60acbee28c21e
ByUs / Kevin Cassier <kevin@byus.fr>
2022-01-19 12:42:13 +0100

    Add front integration

---
+++ src/integrations/front.js
@@ -0,0 +1,20 @@
+clockifyButton.render('.conversationTopbar__StyledConversationTopbar-sc-denlcl-0:not(.clockify)',
+    {observe: true},
+    (elem) => {
+        const titleSelector = $('.conversationSubjectWrapper__StyledSubjectContainerDiv-sc-1xaczhh-0 > div > span');
+        const recipientSelector = $('.messageViewerRecipient__StyledDetailsWrapperDiv-sc-1z03g13-1 div');
+        const title = titleSelector ? titleSelector.textContent : null;
+        const recipient = recipientSelector ? recipientSelector.textContent : null;
+        const taskName = '[Ticket] ' + title + ' - ' + recipient;
+        const container = $('.conversationTopbar__StyledContentDiv-sc-denlcl-1');
+        const description = taskName;
+
+        link = clockifyButton.createButton({
+            description,
+            projectName: null,
+            taskName
+        });
+        link.style.marginLeft = "10px";
+        container.appendChild(link);
+    }
+);
