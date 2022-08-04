6e3621be693c5819f66c0dc2d62200ffff2c4207
Roni Laukkarinen <roni@dude.fi>
2022-08-04 12:36:22 +0300

    Fix help scout icon alignment

--- src/integrations/helpscout.js
+++ src/integrations/helpscout.js
@@ -7,10 +7,24 @@ clockifyButton.render('.convo-actions:not(.clockify)', { observe: true }, () => 
       $('#subjectLine').textContent;
 
     link = clockifyButton.createSmallButton(description);
-    link.style.marginTop = "10px";
 
     var listItem = document.createElement('li');
     listItem.appendChild(link);
 
     $('.convo-actions').appendChild(listItem);
+
+    // Fix alignment
+    document.head.insertAdjacentHTML("beforeend",
+    `<style type="text/css">
+      a.small.clockify-button-active,
+      a.small.clockify-button-inactive {
+        margin: 0;
+      }
+
+      .convo-actions > li:last-child {
+        display: inline-flex;
+        justify-content: flex-start;
+        align-items: center;
+      }
+    </style>`)
 });
