51291bb566fa9ba9b6a6462097febed4d1b8f3c8
Joffrey Létuvé <joffrey.letuve@arexys.com>
2023-03-06 19:01:21 +0100

    Debug lines suppression

--- src/integrations/zohoprojects.js
+++ src/integrations/zohoprojects.js
@@ -3,14 +3,11 @@ setTimeout(() => {
         const taskName = $(".detail-title-plain").innerText;
         const projectTitle = $(".detail-updates i.zoho-projects").innerText;
 
-        console.log("Clockify Zoho Projects : "+projectTitle +" > " + taskName);
-
         const link = clockifyButton.createButton({
             description: taskName,
             projectName: projectTitle,
             small: false
         });
-        //link.style.paddingBottom = "25px";
         elem.append(link);
 
     });
