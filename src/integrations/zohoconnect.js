ba754207924d270e2dd84fc137cf8b6816cd90f2
Joffrey <tazoony@users.noreply.github.com>
2023-06-30 11:15:59 +0200

    Add support for Zoho Projects and Zoho Connect (#234)
    * Add support for Zoho Connect tasks
    
    * Integration with Zoho Projects
    
    * Debug lines suppression
    
    ---------
    
    Co-authored-by: stefancoing <107914814+stefancoing@users.noreply.github.com>

---
+++ src/integrations/zohoconnect.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+    clockifyButton.render('.zc-taskInformation:not(.clockify)', { observe: true }, function (elem) {
+        const taskName = $(".zc-popupTaskTitle").innerText;
+        const boardName = $(".zc-taskProjectInfo").innerText;
+
+        const link = clockifyButton.createButton({
+            description: taskName,
+            projectName: boardName,
+            small: false
+        });
+        link.style.paddingBottom = "25px";
+        elem.prepend(link);
+
+    });
+}, 1000);
