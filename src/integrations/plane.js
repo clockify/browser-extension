4052d4982c3931f2da333e69fbf1152559b4a962
Maksym Mospanenko <mmospanenko@gmail.com>
2024-03-05 02:09:02 +0200

    Add plane task details button

---
+++ src/integrations/plane.js
@@ -0,0 +1,15 @@
+// Details - v0.15.4-dev
+clockifyButton.render(
+	'.w-full .text-base:not(.clockify)',
+	{ observe: true },
+	async (elem) => {
+		const description = () => $('*[placeholder="Issue title"]', elem.parentNode).textContent;
+		const taskName = () => elem.textContent;
+		const projectName = () => $('.line-clamp-1').textContent;
+		const link = clockifyButton.createSmallButton({ description, taskName, projectName });
+		link.style.marginLeft = '2px';
+		link.style.bottom = '-2px';
+		link.style.position = 'relative';
+		elem.append(link);
+	}
+);
