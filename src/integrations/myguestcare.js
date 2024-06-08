664acd1fd5201c6697891d13261add64a55edd1d
Gianluca <g.mereu@gmail.com>
2024-06-08 03:55:08 +0200

    Get info from localstorage or sessionstorage

--- src/integrations/myguestcare.js
+++ src/integrations/myguestcare.js
@@ -3,17 +3,15 @@
 		'.navbar-top-name',
 		{ observe: false },
 		async (elem) => {
-
 			const container = $('.navbar-top-name .change-client');
 			const tenantCode = $('.navbar-top-name>span').textContent;
-
 
 			const description = `[${tenantCode}] `;
 
 			const link = clockifyButton.createButton({
+				...localStorage.getItem('clockify_defaults'),
+				...sessionStorage.getItem('clockify_defaults'),
 				description,
-				projectName: 'SUP - Supporto Telefonico' ,
-				taskName: 'Generico',
 				small: false,
 			});
 			container.append(link);
