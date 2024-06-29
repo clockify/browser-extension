bbcb3b3392c635487f4a8047be0a40afea93d8a7
Mikhail Chemezov <michlenanosoft@gmail.com>
2024-06-29 06:03:59 +0500

    added pyrus.com integration.

---
+++ src/integrations/pyrus.js
@@ -0,0 +1,65 @@
+clockifyButton.render(
+	'.sideBySideHeader:not(.clockify)',
+	{ observe: true },
+	(itemHeader) => {
+		const titleSelector = '.sideBySideHeader__title';
+		const tagsSelector = '.badgeList__item';
+
+		const itemTitle = () => text(titleSelector, itemHeader);
+		const itemId = () => {
+			let hash = document.location.hash;
+			const regex = /(\d+)$/;
+
+			if (hash.startsWith('#id') || hash.startsWith('#list')) {
+				const match = hash.match(regex);
+
+				if (match && match[0]) {
+					return match[0];
+				}
+			}
+		}
+
+		const description = () => createDescription({ itemId, itemTitle });
+		const projectName = () => {
+			let title = itemTitle();
+
+			const regex = /\[(.*?)\]/;
+			const match = title.match(regex);
+
+			if (match && match[1]) {
+				return match[1];
+			}
+
+			return null;
+		}
+
+		const taskName = () => itemTitle();
+		const tagNames = () => textList(tagsSelector, itemHeader);
+
+		const entry = { description, projectName, taskName, tagNames };
+
+		const link = clockifyButton.createButton(entry);
+
+		const container = createTag('div', 'clockify-widget-container');
+
+		container.append(link);
+
+		$('.sideBySideHeader__bottom', itemHeader).append(container);
+	}
+);
+
+function createDescription({ itemId, itemTitle }) {
+	if (!itemId()) return itemTitle();
+
+	return `${itemTitle()} (#${itemId()})`;
+}
+
+applyStyles(`
+	.clockify-widget-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		gap: 15px;
+		padding: 5px 0;
+	}
+`);
